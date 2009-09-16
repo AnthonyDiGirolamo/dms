38ac31e8b3d53d471a0f3c4e3edb7085e02351f0
adigiro <anthony.d@asu.edu>
2009-09-16 10:30:04 -0700

    Scaffold for documents

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+#  map.resources :documents
+
   map.home '', :controller => 'home', :action => 'index'
   map.home '/', :controller => 'home', :action => 'index'
 
