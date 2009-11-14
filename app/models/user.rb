8015b9176479c03df31bd7e4bbe02ffa27b95bc1
adigiro <anthony.d@asu.edu>
2009-11-13 21:38:04 -0700

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :user_requests
   has_many :documents
   has_many :audits
-  has_many :shares #, :class_name => "Share", :foreign_key => "owner_id"
+  has_many :shares, :class_name => "Share", :foreign_key => "owner_id"
   has_and_belongs_to_many :departments
 
   include Authentication
